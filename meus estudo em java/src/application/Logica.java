ce8a754535da4444a52b00975db62bde347616d7
Elisson Braz <59119081+ElissonBraz@users.noreply.github.com>
2023-05-23 21:24:10 -0300

    Update Logica.java

--- meus estudo em java/src/application/Logica.java
+++ meus estudo em java/src/application/Logica.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Logica {
     public static void main(String[] args) {
-       Scanner leia = new Scanner(System.in);
+        Scanner leia = new Scanner(System.in);
 
         leia.close();
     }
