ad143e2832f819f5da8164d67f76d390d67491c7
Elisson Braz <59119081+ElissonBraz@users.noreply.github.com>
2023-06-11 21:19:28 -0300

    Programa Rent

--- meus estudo em java/src/application/ProgramRent.java
+++ meus estudo em java/src/application/ProgramRent.java
@@ -1,5 +1,36 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Rent;
+
 public class ProgramRent {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+         Scanner ent = new Scanner(System.in);
+
+         Rent[] vect = new Rent[10];
+
+         System.out.print("How many rooms will be rented? ");
+         int n = ent.nextInt();
+
+         for (int i = 1; i <= n; i++) {
+            System.out.println("Rent #" + i + ":");
+            System.out.print("Name: ");
+            String name = ent.next();
+            System.out.print("E-mail: ");
+            String email = ent.next();
+            System.out.print("Room: ");
+            int roomNumber = ent.nextInt();
+            vect[roomNumber] = new Rent(name, email);
+         }
+
+
+         ent.close();
+    }
+
     
 }
