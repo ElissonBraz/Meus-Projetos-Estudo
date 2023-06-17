5ec10caf50768ae531a5c74ef0ebd64ede5b9fb0
Elisson Braz <59119081+ElissonBraz@users.noreply.github.com>
2023-06-17 11:01:40 -0300

    aula 4

---
+++ meus estudo em java/src/application/ProgramCaneta.java
@@ -0,0 +1,10 @@
+package application;
+
+import entities.Caneta;
+
+public class ProgramCaneta {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("BIC", "AZUL", 0.5f);
+        c1.status();
+    }
+}
