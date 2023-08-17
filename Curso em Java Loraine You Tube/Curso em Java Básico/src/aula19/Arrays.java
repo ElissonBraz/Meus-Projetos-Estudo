c2a77e19a63a9bd9625c51b78cca470926d36f0f
Elisson Braz <59119081+ElissonBraz@users.noreply.github.com>
2023-08-16 22:20:16 -0300

    Create Arrays.java

---
+++ Curso em Java Loraine You Tube/Curso em Java Básico/src/aula19/Arrays.java
@@ -0,0 +1,19 @@
+package aula19;
+
+public class Arrays {
+    
+    public static void main(String[] args) {
+        
+        double tempDia001 = 31.3;
+        double tempDia002 = 32;
+        double tempDia003 = 33.7;
+        double tempDia004 = 34;
+        double tempDia005 = 33.1;
+        double tempDia006 = 32.8;
+        double tempDia007 = 30;
+        double tempDia008 = 29.8;
+        double tempDia009 = 31.5;
+
+        double [] temperaturas = new double[365];
+    }
+}
