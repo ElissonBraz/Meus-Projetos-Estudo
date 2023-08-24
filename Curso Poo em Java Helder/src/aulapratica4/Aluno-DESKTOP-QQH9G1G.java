c31cd47e05ee54327d3c8473df52a116c7dfec59
Elisson Braz <59119081+ElissonBraz@users.noreply.github.com>
2023-08-24 19:48:00 -0300

    atualização

---
+++ Curso Poo em Java Helder/src/aulapratica4/Aluno-DESKTOP-QQH9G1G.java
@@ -0,0 +1,63 @@
+package aulapratica4;
+
+public class Aluno extends Pessoa implements SituacaoAcademica {
+    
+    private double nota1,nota2,media;
+
+    public Aluno(String nome, String bairro, double nota1, double nota2) {
+        super(nome, bairro);
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    @Override
+    public String verificacaoAprovacao() {
+        if (this.media >= 7) {
+            return "Aprovado";
+        } else {
+            return "Reprovado";
+        }
+    }
+
+    @Override
+    public double calcularMedia() {
+        this.media = (this.nota1 + this.nota2) / 2;
+        return this.media;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " com a Nota 1 - " 
+        + this.nota1 + " e a Nota 2 - "
+        + this.nota2 
+        + " teve a Média Total: " 
+        + media + "";
+    }
+
+    
+    
+}
